82814943e6dfdbb6960b5a09d467a31d83770e60
KyleHouston84 <chariot124@gmail.com>
2022-01-12 00:23:19 -0800

    Added firestore sign up functionality

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,5 +1,8 @@
 import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
+import { getAuth, createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
+import { setDoc, doc, serverTimestamp } from 'firebase/firestore';
+import { db } from '../firebase.config';
 import { ReactComponent as ArrowRightIcon } from '../assets/svg/keyboardArrowRightIcon.svg';
 import visibilityIcon  from '../assets/svg/visibilityIcon.svg';
 
@@ -21,6 +24,33 @@ function SignUp() {
     }))
   };
 
+  const onSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const auth = getAuth();
+
+      const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+
+      const user = userCredential.user;
+
+      updateProfile(auth.currentUser, {
+        displayName: name,
+      });
+
+      const formDataCopy = {...formData};
+      delete formDataCopy.password;
+      formDataCopy.timestamp = serverTimestamp();
+
+      await setDoc(doc(db, 'users', user.uid), formDataCopy);
+
+      navigate('/');
+
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
   return (
     <>
       <div className="pageContainer">
@@ -31,7 +61,7 @@ function SignUp() {
         </header>
 
         <main>
-          <form>
+          <form onSubmit={onSubmit}>
             <input type="text" className="nameInput" placeholder='name' id='name' value={name} onChange={onChange}/>
             <input type="email" className="emailInput" placeholder='email' id='email' value={email} onChange={onChange}/>
             <div className="passwordInputDiv">
