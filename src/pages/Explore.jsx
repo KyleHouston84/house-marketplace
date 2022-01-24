d70467cc7f428b14b4ce13af56082d4a6c3ce57c
KyleHouston84 <chariot124@gmail.com>
2022-01-23 22:32:41 -0800

    Initial commit for the categories page

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -1,7 +1,30 @@
+import { Link } from "react-router-dom";
+import rentCategoryImage from '../assets/jpg/rentCategoryImage.jpg';
+import sellCategoryImage from '../assets/jpg/sellCategoryImage.jpg';
+
 function Explore() {
   return (
-    <div>
-      <h1>Explore</h1>
+    <div className="explore">
+      <header>
+        <p className="pageHeader">Explore</p>
+      </header>
+     
+      {/* Slider */}
+
+      <main>
+        <p className="exploreCategoryHeading">Categories</p>
+        <div className="exploreCategories">
+          <Link to='/category/rent'>
+            <img className="exploreCategoryImg" src={rentCategoryImage} alt="rent" />
+            <p className="exploreCategoryName">Places for rent</p>
+          </Link>
+          
+          <Link to='/category/sell'>
+            <img className="exploreCategoryImg" src={sellCategoryImage} alt="sell" />
+            <p className="exploreCategoryName">Places for sale</p>
+          </Link>
+        </div>
+      </main>
     </div>
   )
 }
