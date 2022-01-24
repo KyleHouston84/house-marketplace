00484baf51392ff1d971acb3277d20e6d1c54cc8
KyleHouston84 <chariot124@gmail.com>
2022-01-23 23:25:26 -0800

    Added listings fetch to the category page

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -19,7 +19,7 @@ function Explore() {
             <p className="exploreCategoryName">Places for rent</p>
           </Link>
           
-          <Link to='/category/sell'>
+          <Link to='/category/sale'>
             <img className="exploreCategoryImg" src={sellCategoryImage} alt="sell" />
             <p className="exploreCategoryName">Places for sale</p>
           </Link>
