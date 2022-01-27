4c3088e69280541fa978653d4816692ed35cab66
KyleHouston84 <chariot124@gmail.com>
2022-01-26 21:37:07 -0800

    initial commit for the create listing page

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,9 +1,11 @@
 import { useState } from 'react';
 import { getAuth, updateProfile } from 'firebase/auth';
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { db } from '../firebase.config';
 import { updateDoc, doc } from 'firebase/firestore';
 import { toast } from 'react-toastify';
+import arrowRight from '../assets/svg/keyboardArrowRightIcon.svg';
+import homeIcon from '../assets/svg/homeIcon.svg';
 
 function Profile() {
   const auth = getAuth();
@@ -72,6 +74,12 @@ function Profile() {
           <input type="text" id="email" className={!changeDetails ? 'profileEmail' : 'profileEmail'} disabled={!changeDetails} value={email} onChange={onChange} disabled={true}/>
         </form>
       </div>
+
+      <Link to='/create-listing' className='createListing'>
+        <img src={homeIcon} alt="home" />
+        <p>Sell or rent your home</p>
+        <img src={arrowRight} alt="arrow right" />
+      </Link>
     </main>
   </div>
 }
